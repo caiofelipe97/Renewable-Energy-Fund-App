1d66f9540c06be321481842116b39b2f8ff93994
caiofelipe97 <caiofelipee17@gmail.com>
2023-09-21 00:31:30 -0300

    Setup the React Native with Typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App);
